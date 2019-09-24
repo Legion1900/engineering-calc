b3e6892cf6ac5f5d6a95b012856aab0529de38a1
Chumachenko Roman <rom4ik730@gmail.com>
2019-09-24 11:36:12 +0300

    Initial commit.

---
+++ app/src/main/java/com/legion1900/engineeringcalculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.legion1900.engineeringcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
