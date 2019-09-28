49830f8fbb939dd28e58dc94252f3beb5257f88f
Chumachenko Roman <rom4ik730@gmail.com>
2019-09-28 18:05:25 +0300

    Added fragment with base operations.

--- app/src/main/java/com/legion1900/engineeringcalculator/view/fragments/BaseOperatorsFragment.kt
+++ app/src/main/java/com/legion1900/engineeringcalculator/view/fragments/BaseOperatorsFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.legion1900.engineeringcalculator.R
 
-class MainNumpadFragment : Fragment() {
+class BaseOperatorsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_numpad, container, false)
+        return inflater.inflate(R.layout.fragment_base_operations, container, false)
     }
 }
