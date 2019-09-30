a5417bcfa0fe6ba4bb5fed964bd84b56e4f1d7de
Chumachenko Roman <rom4ik730@gmail.com>
2019-09-30 16:04:47 +0300

    Rebuild extended keyboard into separate fragment.

---
+++ app/src/main/java/com/legion1900/engineeringcalculator/view/fragments/ExtendedKeyboardFragment.kt
@@ -0,0 +1,20 @@
+package com.legion1900.engineeringcalculator.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.legion1900.engineeringcalculator.R
+
+class ExtendedKeyboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_extended_keyboard, container, false)
+    }
+
+}
