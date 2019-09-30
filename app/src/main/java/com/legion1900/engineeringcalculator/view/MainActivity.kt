24e199a915cf8bd080b8519ba2c2d6d04cbd9d72
Chumachenko Roman <rom4ik730@gmail.com>
2019-09-30 12:38:41 +0300

    Added EditText font "autosize".

--- app/src/main/java/com/legion1900/engineeringcalculator/view/MainActivity.kt
+++ app/src/main/java/com/legion1900/engineeringcalculator/view/MainActivity.kt
@@ -2,12 +2,24 @@ package com.legion1900.engineeringcalculator.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.legion1900.engineeringcalculator.R
+import com.legion1900.engineeringcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        adjustEditTextSize()
+    }
+
+    fun adjustEditTextSize() {
+        with(binding) {
+            etInput.textSize = tvStory.textSize
+        }
     }
 }
